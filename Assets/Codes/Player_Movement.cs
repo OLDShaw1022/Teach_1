e7cf23a98a77e1356092558e935f074748068a90
OLDShaw1022 <73062433+OLDShaw1022@users.noreply.github.com>
2022-04-02 15:13:02 +0800

    Description
    Added some description in Player_Movement script.

--- Assets/Codes/Player_Movement.cs
+++ Assets/Codes/Player_Movement.cs
@@ -16,13 +16,13 @@ public class Player_Movement : MonoBehaviour
     //logic
     bool isGrounded;
 
-
+    //Do when system start
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    
+    //Update every screen frame
     void Update()
     {
         //Input
@@ -36,17 +36,20 @@ public class Player_Movement : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
+    //Update every certain frame
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
     }
 
+    //Jump function
     void Jump()
     {
         Vector2 jumpVec = new Vector2(0, 1 * jumpForce);
         rb.AddForce(jumpVec, ForceMode2D.Impulse);
     }
 
+    //Draw something on editor screen when selected
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
