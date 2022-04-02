cc6209b68b192163db2b1231feaa887be8df6fb9
OLDShaw1022 <73062433+OLDShaw1022@users.noreply.github.com>
2022-04-02 15:00:54 +0800

    Lesson_1
    Added some basic movement scripts.

---
+++ Assets/Codes/Player_Movement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    //value
+    public float speed = 5f;
+    public float jumpForce;
+    Vector2 movement;
+    Rigidbody2D rb;
+
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    //logic
+    bool isGrounded;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            Jump();
+        }
+
+        //Logic
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        Vector2 jumpVec = new Vector2(0, 1 * jumpForce);
+        rb.AddForce(jumpVec, ForceMode2D.Impulse);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
